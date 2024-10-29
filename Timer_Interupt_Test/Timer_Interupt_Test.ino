a5276a5afd314794716b68f7bd1d8ac81c8f8ebd
Aldino Arya Pramana <82914293+Aldinoooop@users.noreply.github.com>
2024-10-29 12:05:13 +0700

    Adding Multiple Timer Function Using Latest ESP32_hal_timer library

--- Timer_Interupt_Test/Timer_Interupt_Test.ino
+++ Timer_Interupt_Test/Timer_Interupt_Test.ino
@@ -3,30 +3,46 @@
 
 // Deklarasi variabel timer
 hw_timer_t *My_timer = NULL;
+hw_timer_t *timer1 = NULL;
+hw_timer_t *timer2 = NULL;
 
 // Fungsi yang akan dipanggil saat interrupt timer terpicu
-void IRAM_ATTR onTimer() {
+void IRAM_ATTR onTimer1() {
   // Tindakan yang akan dilakukan saat interrupt terpicu
-  Serial.println("Timer Interrupt Triggered");
+  Serial.println("Timer1 Interrupt Triggered");
+}
+
+void IRAM_ATTR onTimer2() {
+  // Tindakan yang akan dilakukan saat interrupt terpicu
+  Serial.println("Timer2 Interrupt Triggered");
 }
 
 void timer_init() {
   // Inisialisasi timer dengan timer 0, prescaler 80 (1 tick = 1 µs)
   // My_timer = timerBegin(0, 80, true);
-  My_timer = timerBegin(1000000);
+  // My_timer = timerBegin(1000000);
+  timer1 = timerBegin(1000000);
+  timer2 = timerBegin(1000000);
 
   // Hubungkan fungsi interrupt ke timer
   // timerAttachInterrupt(My_timer, &onTimer, true);
-  timerAttachInterrupt(My_timer, &onTimer);
+  // timerAttachInterrupt(My_timer, &onTimer);
+  timerAttachInterrupt(timer1, &onTimer1);
+  timerAttachInterrupt(timer2, &onTimer2);
 
   // Atur alarm timer untuk memicu interrupt setiap 12.000 µs (12 ms)
-  timerAlarm(My_timer, 12000, true,0); 
+  timerAlarm(timer1, 1000000, true, 0);
+  timerAlarm(timer2, 2000000, true, 0);
   // timerAlarmWrite(My_timer, 12000, true);
 
   // // Aktifkan alarm timer
   // timerAlarmEnable(My_timer);
 
   Serial.println("Timer initialized");
+  // timerStart(timer1);
+  // timerStart(timer2);
+  // timerWrite(timer1, 0);
+  // timerWrite(timer2, 0);
 }
 
 void setup() {
@@ -38,5 +54,6 @@ void setup() {
 }
 
 void loop() {
+  // Serial.println("Timer 1 = " + String(timerRead(timer1)) + " Timer 2 = " + String(timerRead(timer2)));
   // Main loop kosong, karena semua fungsi ditangani oleh interrupt
 }
